4a29b2cde7d8a2c6f2bf7eed4b0dd4709fe62633
Lim Chee Kin <limcheekin@vobject.com>
2021-04-13 10:59:06 +0800

    added by owner link to showcase view

--- showcase_view/lib/showcase_view.dart
+++ showcase_view/lib/showcase_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/link.dart';
 import 'license_view.dart';
 import 'read_me_view.dart';
 import 'package:source_code_view/source_code_view.dart';
@@ -21,6 +22,7 @@ class ShowcaseView extends StatelessWidget {
   final bool showLicense;
   final List<Tab> additionalTabs;
   final List<Widget> additionalTabBarViews;
+  static const GITHUB_URL = 'https://github.com/';
 
   const ShowcaseView({
     @required this.title,
@@ -135,11 +137,46 @@ class ShowcaseView extends StatelessWidget {
                   unselectedLabelColor: Colors.black,
                   tabs: tabs,
                 ),
-                title: Text(
-                  title,
-                  style: TextStyle(
-                    fontSize: 28.0,
-                    color: Colors.black,
+                title: Padding(
+                  padding: const EdgeInsets.only(top: 15.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        title,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      Link(
+                        uri: Uri.parse(GITHUB_URL + owner),
+                        builder:
+                            (BuildContext context, FollowLink followLink) =>
+                                Row(
+                          children: [
+                            Text(
+                              'By ',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 13.0,
+                              ),
+                            ),
+                            TextButton(
+                              onPressed: followLink,
+                              child: Text(
+                                owner,
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  decoration: TextDecoration.underline,
+                                  fontSize: 13.0,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
                   ),
                 ),
                 backgroundColor: Colors.white,
