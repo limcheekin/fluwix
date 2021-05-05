80eb3aa28f4b4e34f4a4b8f17b80f5d8cc4b3e51
Lim Chee Kin <limcheekin@vobject.com>
2021-05-05 16:49:47 +0800

    first import of neumorphism_design module

---
+++ neumorphism_design/README.md
@@ -0,0 +1,11 @@
+# neumorphism_design
+
+Please see the [README](https://github.com/Idean/Flutter-Neumorphic/blob/master/README.md) of the flutter_neumorphic.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.dev/).
+
+For instructions integrating Flutter modules to your existing applications,
+see the [add-to-app documentation](https://flutter.dev/docs/development/add-to-app).
---
+++ neumorphism_design/lib/neumorphism_design_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:showcase_view/showcase_view.dart';
+
+import 'neumorphism_design_widget.dart';
+
+class NeumorphismDesignScreen extends StatelessWidget {
+  const NeumorphismDesignScreen({Key key}) : super(key: key);
+
+  static const owner = 'Idean';
+  static const repository = 'Flutter-Neumorphic';
+  static const branch = 'master';
+
+  @override
+  Widget build(BuildContext context) {
+    return ShowcaseView(
+      title: 'Neumorphism Design',
+      widget: FullSampleHomePage(),
+      owner: owner,
+      repository: repository,
+      ref: branch,
+      readMe: 'README.md',
+      codePaths: [
+        'example/pubspec.yaml',
+        'example/lib/main.dart',
+        'example/lib/main_home.dart',
+      ],
+    );
+  }
+}
