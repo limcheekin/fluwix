0499b3bbf073d47c6289e63bc6f65cab065bd54a
Lim Chee Kin <limcheekin@vobject.com>
2021-06-03 17:23:55 +0800

    added logger to _GetNumberTriviaObserver class

--- number_trivia/lib/presentation/pages/number_trivia_presenter.dart
+++ number_trivia/lib/presentation/pages/number_trivia_presenter.dart
@@ -2,6 +2,7 @@ import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 import 'package:number_trivia/domain/entities/number_trivia.dart';
 import 'package:number_trivia/domain/usecases/get_concrete_number_trivia.dart';
 import 'package:number_trivia/domain/usecases/get_random_number_trivia.dart';
+import 'package:logging/logging.dart';
 
 class NumberTriviaPresenter extends Presenter {
   final GetConcreteNumberTrivia getConcreteNumberTrivia;
@@ -32,12 +33,13 @@ class NumberTriviaPresenter extends Presenter {
 }
 
 class _GetNumberTriviaObserver extends Observer<NumberTrivia> {
+  final logger = Logger('_GetNumberTriviaObserver');
   final NumberTriviaPresenter presenter;
+
   _GetNumberTriviaObserver(this.presenter);
   @override
   void onComplete() {
-    print(
-        'presenter.getNumberTriviaOnComplete ${presenter.getNumberTriviaOnComplete}');
+    logger.finest('onComplete()');
     if (presenter.getNumberTriviaOnComplete != null) {
       presenter.getNumberTriviaOnComplete!();
     }
@@ -45,6 +47,7 @@ class _GetNumberTriviaObserver extends Observer<NumberTrivia> {
 
   @override
   void onError(e) {
+    logger.finest('onError($e)');
     if (presenter.getNumberTriviaOnError != null) {
       presenter.getNumberTriviaOnError!(e);
     }
@@ -52,6 +55,7 @@ class _GetNumberTriviaObserver extends Observer<NumberTrivia> {
 
   @override
   void onNext(response) {
+    logger.finest('onNext($response)');
     if (presenter.getNumberTriviaOnNext != null) {
       presenter.getNumberTriviaOnNext!(response);
     }
