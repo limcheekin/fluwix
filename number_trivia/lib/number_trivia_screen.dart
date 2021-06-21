56572569c3343ffd15fc2a741f1c612d495cd448
Lim Chee Kin <limcheekin@vobject.com>
2021-06-21 12:06:48 +0800

    feat: added test tab

--- number_trivia/lib/number_trivia_screen.dart
+++ number_trivia/lib/number_trivia_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:showcase_view/showcase_view.dart';
+import 'package:source_code_view/source_code_view.dart';
 
 import 'presentation/pages/number_trivia_view.dart';
 
@@ -11,6 +11,7 @@ class NumberTriviaScreen extends StatelessWidget {
   static const repository = 'fluwix';
   static const branch = 'number_trivia_flutter_clean_architecture';
   static const srcDir = 'number_trivia/lib';
+  static const testDir = 'number_trivia/test';
 
   @override
   Widget build(BuildContext context) {
@@ -41,6 +42,28 @@ class NumberTriviaScreen extends StatelessWidget {
         '$srcDir/data/repositories/number_trivia_repository_impl.dart',
         '$srcDir/common/network/network_info.dart',
         '$srcDir/common/presentation/input_converter.dart',
+      ],
+      additionalTabs: [
+        Tab(text: 'Test'),
+      ],
+      additionalTabBarViews: [
+        SingleChildScrollView(
+          child: SourceCodeView(
+            owner: owner,
+            repository: repository,
+            ref: branch,
+            paths: [
+              '$testDir/fixtures/fixture_reader.dart',
+              '$testDir/domain/usecases/get_concrete_number_trivia_test.dart',
+              '$testDir/domain/usecases/get_random_number_trivia_test.dart',
+              '$testDir/data/repositories/number_trivia_repository_impl_test.dart',
+              '$testDir/data/datasources/number_trivia_local_data_source_test.dart',
+              '$testDir/data/datasources/number_trivia_remote_data_source_test.dart',
+              '$testDir/common/network/network_info_test.dart',
+              '$testDir/common/presentation/input_converter_test.dart',
+            ],
+          ),
+        ),
       ],
     );
   }
