88018bc9dd0926b9a3c2bfaaffaab0d6db2bcb36
Lim Chee Kin <limcheekin@vobject.com>
2023-07-06 15:20:13 +0800

    feat: reported code issues fixed by dart fix --apply

--- chatbot_ui/lib/widgets/chat_widget.dart
+++ chatbot_ui/lib/widgets/chat_widget.dart
@@ -69,10 +69,10 @@ class ChatWidget extends StatelessWidget {
                 ),
                 chatIndex == 0
                     ? const SizedBox.shrink()
-                    : Row(
+                    : const Row(
                         mainAxisAlignment: MainAxisAlignment.end,
                         mainAxisSize: MainAxisSize.min,
-                        children: const [
+                        children: [
                           Icon(
                             Icons.thumb_up_alt_outlined,
                             color: Colors.white,
