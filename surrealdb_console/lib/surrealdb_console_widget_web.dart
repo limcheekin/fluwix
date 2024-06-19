e8f86e8d4cc88460635df498ac370326523ed33c
Lim Chee Kin <limcheekin@vobject.com>
2024-06-19 10:29:30 +0800

    fix: Don't use 'BuildContext's across async gaps

--- surrealdb_console/lib/surrealdb_console_widget_web.dart
+++ surrealdb_console/lib/surrealdb_console_widget_web.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:console/console.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
