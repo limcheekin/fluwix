61c6d0a3657f348069d92e2235af853d71685947
limcheekin <limcheekin@vobject.com>
2025-07-27 12:38:58 +0800

    chore: upgrade the ndkVersion to latest

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.fluwix"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "27.3.13750724"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
