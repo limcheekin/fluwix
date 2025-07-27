ae508d3238f23409c7a170290dd4c8c8db7f1842
limcheekin <limcheekin@vobject.com>
2025-07-27 12:10:14 +0800

    chore: remove wallet_connect_showcase module which blocking android release

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -8,17 +8,6 @@ allprojects {
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
-    afterEvaluate {
-        // Only configure if the android extension is present
-        extensions.findByName("android")?.let { ext ->
-            // Cast the extension to the correct type
-            (ext as? com.android.build.gradle.BaseExtension)?.apply {
-                this.namespace = "com.fluwix"
-            }
-        }
-    }
-}
-subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
